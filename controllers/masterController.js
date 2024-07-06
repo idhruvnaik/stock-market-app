73d2076a43bd596a8e1279b211b39263a846bf33
devanshu-fitterfly <107918529+fitterfly-devanshu@users.noreply.github.com>
2024-07-06 04:19:11 +0000

    add: local storage to symbols list api

--- controllers/masterController.js
+++ controllers/masterController.js
@@ -1,14 +1,27 @@
+var LocalStorage = require('node-localstorage').LocalStorage;
+localStorage = new LocalStorage('../storage');
+
 const symbols = async (_, res) => {
-    try {
+  try {
+    const symbolsData = JSON.parse(localStorage.getItem('symbols'))
+    const now = new Date()
+    const lastUpdatedDateTime = new Date(localStorage.getItem('lastUpdated') || "2000")
+    const todaysUpdateDateTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 3, 30)
+
+    if (symbolsData && lastUpdatedDateTime > todaysUpdateDateTime) {
+      res.status(200).json(symbolsData)
+    } else {
       const response = await fetch('https://margincalculator.angelbroking.com/OpenAPI_File/files/OpenAPIScripMaster.json')
       const allSymbols = await response.json();
       const filteredSymbols = allSymbols.filter(isFUTSymbol)
-      console.log(filteredSymbols.length)
-      res.status(200).json(filteredSymbols);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
+      localStorage.setItem('symbols', JSON.stringify(filteredSymbols))
+      localStorage.setItem('lastUpdated', new Date().toISOString())
+      res.status(200).json(filteredSymbols)
     }
-  };
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 const isFUTSymbol = (symbol) => symbol.exch_seg == 'NFO' && symbol.instrumenttype == 'FUTSTK'
 
