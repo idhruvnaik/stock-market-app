e816e5dc42f659e67b4c8268f8ff76f1be0d37f3
devanshu-fitterfly <107918529+fitterfly-devanshu@users.noreply.github.com>
2024-07-07 11:53:12 +0530

    fix: move symbols functions to utils

--- controllers/masterController.js
+++ controllers/masterController.js
@@ -1,29 +1,9 @@
 var LocalStorage = require('node-localstorage').LocalStorage;
 localStorage = new LocalStorage('./storage');
-
-const getFilteredSymbolList = async () => {
-  try {
-    const symbolsData = JSON.parse(localStorage.getItem('symbols'))
-    const now = new Date()
-    const lastUpdatedDateTime = new Date(localStorage.getItem('lastUpdated') || "2000")
-    const todaysUpdateDateTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 3, 30)
-    if (symbolsData && lastUpdatedDateTime > todaysUpdateDateTime) {
-      return symbolsData
-    } else {
-      const response = await fetch('https://margincalculator.angelbroking.com/OpenAPI_File/files/OpenAPIScripMaster.json')
-      const allSymbols = await response.json();
-      const filteredSymbols = allSymbols.filter(isFUTSymbol)
-      localStorage.setItem('symbols', JSON.stringify(filteredSymbols))
-      localStorage.setItem('lastUpdated', new Date().toISOString())
-      return filteredSymbols
-    }
-  } catch (error) {
-    return error.message
-  }
-}
+symbol = require('../utils/symbol')
 
 const symbols = async (_, res) => {
-  const symbolsListOrError = await getFilteredSymbolList()
+  const symbolsListOrError = await symbol.getFilteredSymbolList()
   if (Array.isArray(symbolsListOrError)) {
     res.status(200).json({ symbols: symbolsListOrError })
   }
@@ -32,6 +12,5 @@ const symbols = async (_, res) => {
   }
 };
 
-const isFUTSymbol = (symbol) => symbol.exch_seg == 'NFO' && symbol.instrumenttype == 'FUTSTK'
 
-module.exports = { symbols, getFilteredSymbolList }
+module.exports = { symbols }
