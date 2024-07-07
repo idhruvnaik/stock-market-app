7e15f2ac208e3d8b09f76b23aadf8e1d52a6d3c4
idhruvnaik <dhruv97naik@gmail.com>
2024-07-07 11:56:07 +0530

    Authentication : Invalid Access Token <> Error Message Fix

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,20 +1,22 @@
-const jwt = require('jsonwebtoken');
-const tokens = require('../utils/token');
+const jwt = require("jsonwebtoken");
+const tokens = require("../utils/token");
 
-const authMiddleware = async(req, res, next) => {
+const authMiddleware = async (req, res, next) => {
   try {
-    const token = req.headers.authorization?.split(' ')[1];
+    const token = req.headers.authorization?.split(" ")[1];
     if (!token) {
-      return res.status(403).json({ message: 'No token provided' });
+      return res.status(403).json({ message: "No token provided" });
     }
     const decoded = await tokens.verifyAccessToken(token);
-    if(decoded?.error) {
-      return res.status(401).json({ message: 'Unauthorized' });
+    if (decoded?.error) {
+      return res.status(401).json({ message: "Unauthorized" });
     }
     req.user = decoded;
     next();
-  }catch(error){
-    res.status(error?.code || 500).json({ message: 'Internal Server Error' });
+  } catch (error) {
+    res
+      .status(error?.code || 500)
+      .json({ message: error?.message || "Internal Server Error" });
   }
 };
 
