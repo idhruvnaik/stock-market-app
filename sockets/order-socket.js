66c2b769c9afec64c8acea372b9f5ee3c1d7f8fd
idhruvnaik <dhruv97naik@gmail.com>
2024-07-28 18:56:54 +0530

    Order - Try <> Catch - Added

--- sockets/order-socket.js
+++ sockets/order-socket.js
@@ -18,49 +18,6 @@ const watchlistWS = new WebSocket.Server({
   port: constants.SOCKETS.PENDING_ORDER,
 });
 
-async function init() {
-  try {
-    pendingOrderDataEmiter = await pendingOrders();
-  } catch (e) {
-    console.log(e);
-  }
-}
-
-// ? Pending Order List
-async function pendingOrders() {
-  const orders = await db.UserOrder.findAll({
-    where: { status: constants.ORDER.STATUS.PENDING },
-  });
-
-  for (const order of orders) {
-    const symbolToken = parseInt(order?.symbol_token);
-    if (!pendingOrderDataEmiter.has(symbolToken)) {
-      pendingOrderDataEmiter.set(symbolToken, []);
-    }
-
-    if (!pendingOrderExecutor.has(symbolToken)) {
-      pendingOrderExecutor.set(symbolToken, [order]);
-    } else {
-      pendingOrderExecutor.get(symbolToken).push(order);
-    }
-  }
-
-  return pendingOrderDataEmiter;
-}
-
-async function channelData(data) {
-  try {
-    if (data?.token) {
-      const cleanedString = data?.token?.replace(/"/g, "");
-      const integerNumber = parseInt(cleanedString, 10);
-      sendDataToClient(integerNumber, data);
-      monitorUserOrders(integerNumber, data);
-    }
-  } catch (e) {
-    throw new Error(e);
-  }
-}
-
 watchlistWS.on("connection", async (ws, req) => {
   const location = url.parse(req.url, true);
   const token = location.query.token;
@@ -82,58 +39,124 @@ watchlistWS.on("connection", async (ws, req) => {
   }
 });
 
-async function updatePendingOrderData(ws, data) {
-  let tokens = await db.UserOrder.findAll({
-    where: {
-      user_token: data["tokenDetails"]["unique_token"],
-      status: constants.ORDER.STATUS.PENDING,
-    },
-    attributes: ["symbol_token"],
-    raw: true,
-  });
+// ? Init Method
+async function init() {
+  try {
+    pendingOrderDataEmiter = await pendingOrders();
+  } catch (e) {
+    console.log(e);
+  }
+}
 
-  tokens = tokens?.map((item) => item?.symbol_token);
-  tokens?.forEach((token) => {
-    if (pendingOrderDataEmiter.has(parseInt(token))) {
-      pendingOrderDataEmiter.get(parseInt(token)).push(ws);
+// ? Pending Order List
+async function pendingOrders() {
+  try {
+    const orders = await db.UserOrder.findAll({
+      where: { status: constants.ORDER.STATUS.PENDING },
+    });
+
+    for (const order of orders) {
+      const symbolToken = parseInt(order?.symbol_token);
+      if (!pendingOrderDataEmiter.has(symbolToken)) {
+        pendingOrderDataEmiter.set(symbolToken, []);
+      }
+
+      if (!pendingOrderExecutor.has(symbolToken)) {
+        pendingOrderExecutor.set(symbolToken, [order]);
+      } else {
+        pendingOrderExecutor.get(symbolToken).push(order);
+      }
     }
-  });
+
+    return pendingOrderDataEmiter;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function channelData(data) {
+  try {
+    if (data?.token) {
+      const cleanedString = data?.token?.replace(/"/g, "");
+      const symbolToken = parseInt(cleanedString, 10);
+
+      sendDataToClient(symbolToken, data);
+      monitorUserOrders(symbolToken, data);
+    }
+  } catch (e) {
+    throw e;
+  }
+}
+
+// ? Adds WebSocket connections to the map when a new connection is established
+async function updatePendingOrderData(ws, data) {
+  try {
+    let tokens = await db.UserOrder.findAll({
+      where: {
+        user_token: data["tokenDetails"]["unique_token"],
+        status: constants.ORDER.STATUS.PENDING,
+      },
+      attributes: ["symbol_token"],
+      raw: true,
+    });
+
+    tokens = tokens?.map((item) => item?.symbol_token);
+    tokens?.forEach((token) => {
+      if (pendingOrderDataEmiter.has(parseInt(token))) {
+        pendingOrderDataEmiter.get(parseInt(token)).push(ws);
+      }
+    });
+  } catch (error) {
+    throw error;
+  }
 }
 
 // ? Data Sender Functions()
 async function sendDataToClient(integerNumber, data) {
-  if (pendingOrderDataEmiter.has(integerNumber)) {
-    const clients = pendingOrderDataEmiter.get(integerNumber);
-    if (clients.length > 0) {
-      clients?.forEach((wsClient) => {
-        data.token = integerNumber || data?.token;
-        wsClient.send(JSON.stringify(data));
-      });
+  try {
+    if (pendingOrderDataEmiter.has(integerNumber)) {
+      const clients = pendingOrderDataEmiter.get(integerNumber);
+      if (clients.length > 0) {
+        clients?.forEach((wsClient) => {
+          data.token = integerNumber || data?.token;
+          wsClient.send(JSON.stringify(data));
+        });
+      }
     }
-  }
+  } catch (error) {}
 }
 
 async function monitorUserOrders(integerNumber, data) {
-  if (pendingOrderExecutor.has(integerNumber)) {
-    const orders = await pendingOrderExecutor.get(integerNumber);
-    for (const order of orders) {
-      const last_traded_price = (
-        parseFloat(data?.last_traded_price) / 100
-      ).toFixed(2);
+  try {
+    if (pendingOrderExecutor.has(integerNumber)) {
+      const orders = await pendingOrderExecutor.get(integerNumber);
+      for (const order of orders) {
+        const last_traded_price = (
+          parseFloat(data?.last_traded_price) / 100
+        ).toFixed(2);
 
-      if (
-        last_traded_price <= order?.reference_price &&
-        order?.state == constants.ORDER.STATE.BUY
-      ) {
-        executeUserOrder(order, last_traded_price);
+        if (
+          (last_traded_price <= order?.reference_price &&
+            order?.state == constants.ORDER.STATE.BUY) ||
+          (last_traded_price >= order?.reference_price &&
+            order?.state == constants.ORDER.STATE.SELL)
+        ) {
+          await executeUserOrder(order, last_traded_price);
+        }
       }
     }
+  } catch (error) {
+    throw error;
   }
 }
 
 async function executeUserOrder(order, price) {
-  if (order) {
-    await order.update({ trigger_price: price });
+  try {
+    if (order) {
+      await order.update({ trigger_price: price });
+    }
+  } catch (error) {
+    throw error;
   }
 }
 
@@ -170,7 +193,11 @@ const placeOrder = async (req, res) => {
       req?.user?.tokenDetails?.unique_token,
       req.body
     );
-    await addOrderInMap(order, req?.user?.tokenDetails?.unique_token); // ** Adds newly added order to map
+
+    if (order?.state == constants.ORDER.MODE.LIMIT) {
+      await addOrderInMap(order, req?.user?.tokenDetails?.unique_token); // ** Adds newly added order to map
+    }
+
     res.status(200).json({ content: order });
   } catch (error) {
     res
@@ -194,79 +221,106 @@ const cancelOrder = async (req, res) => {
   }
 };
 
-// ? Manage Map Variables
-async function addOrderInMap(order, user_token) {
-  const symbol_token = parseInt(order?.symbol_token);
-  const ws = await getWs(user_token);
+// !! Map modifier methods
 
-  if (!pendingOrderDataEmiter.has(symbol_token)) {
-    pendingOrderDataEmiter.set(symbol_token, [ws]);
-  } else {
-    if (ws) {
-      const webSockets = pendingOrderDataEmiter?.get(symbol_token);
-      if (!isWsExist(webSockets, ws)) {
-        pendingOrderDataEmiter?.get(symbol_token)?.push(ws);
+// ? Add to Order Map
+async function addOrderInMap(order, user_token) {
+  try {
+    const symbol_token = parseInt(order?.symbol_token);
+    const ws = await getWs(user_token);
+
+    if (!pendingOrderDataEmiter.has(symbol_token)) {
+      pendingOrderDataEmiter.set(symbol_token, [ws]);
+    } else {
+      if (ws) {
+        const webSockets = pendingOrderDataEmiter?.get(symbol_token);
+        if (!isWsExist(webSockets, ws)) {
+          pendingOrderDataEmiter?.get(symbol_token)?.push(ws);
+        }
       }
     }
-  }
 
-  if (!pendingOrderExecutor.has(symbol_token)) {
-    pendingOrderExecutor.set(symbol_token, [order]);
-  } else {
-    pendingOrderExecutor.get(symbol_token).push(order);
+    if (!pendingOrderExecutor.has(symbol_token)) {
+      pendingOrderExecutor.set(symbol_token, [order]);
+    } else {
+      pendingOrderExecutor.get(symbol_token).push(order);
+    }
+  } catch (error) {
+    throw error;
   }
 }
 
 // ? Remove From Order Map
 async function removeOrderFromMap(order, user_token) {
-  const symbol_token = parseInt(order?.symbol_token);
-  const ws = await getWs(user_token);
+  try {
+    const symbol_token = parseInt(order?.symbol_token);
+    const ws = await getWs(user_token);
 
-  if (ws) {
-    let webSockets = pendingOrderDataEmiter?.get(symbol_token);
-    webSockets = webSockets.filter(
-      (client) => client.unique_token != ws?.unique_token
+    if (ws) {
+      let webSockets = pendingOrderDataEmiter?.get(symbol_token);
+      webSockets = webSockets.filter(
+        (client) => client.unique_token != ws?.unique_token
+      );
+      pendingOrderDataEmiter?.set(symbol_token, webSockets);
+    }
+
+    let orders = pendingOrderExecutor.get(symbol_token);
+    orders = orders.filter(
+      (object) => object.order_token !== order?.order_token
     );
-    pendingOrderDataEmiter?.set(symbol_token, webSockets);
+    pendingOrderExecutor?.set(symbol_token, orders);
+  } catch (error) {
+    throw error;
   }
-
-  let orders = pendingOrderExecutor.get(symbol_token);
-  orders = orders.filter((object) => object.order_token !== order?.order_token);
-  pendingOrderExecutor?.set(symbol_token, orders);
 }
 
 // ? Update Order Map
 async function updateOrderMap(order, user_token) {
-  const symbol_token = parseInt(order?.symbol_token);
-  let orders = pendingOrderExecutor.get(symbol_token);
+  try {
+    const symbol_token = parseInt(order?.symbol_token);
+    let orders = pendingOrderExecutor.get(symbol_token);
 
-  orders = orders.filter((object) => object.order_token !== order?.order_token);
+    orders = orders.filter(
+      (object) => object.order_token !== order?.order_token
+    );
 
-  pendingOrderExecutor?.set(symbol_token, orders);
+    pendingOrderExecutor?.set(symbol_token, orders);
+  } catch (error) {
+    throw error;
+  }
 }
 
 // ? Returns a single WebSocket connection based on the given user_token
 async function getWs(user_token) {
-  const webSockets = watchlistWS?.clients || [];
-  let connection = null;
-  for (const ws of webSockets) {
-    if (ws.unique_token == user_token) {
-      connection = ws;
-      break;
+  try {
+    const webSockets = watchlistWS?.clients || [];
+    let connection = null;
+    for (const ws of webSockets) {
+      if (ws.unique_token == user_token) {
+        connection = ws;
+        break;
+      }
     }
+    return connection;
+  } catch (error) {
+    throw error;
   }
-  return connection;
 }
 
+// ? Checks if a particular client exists in WebSocket clients
 async function isWsExist(webSockets, newWs) {
-  let isExist = false;
-  for (const ws of webSockets) {
-    if (ws?.unique_token == newWs?.unique_token) {
-      isExist = true;
+  try {
+    let isExist = false;
+    for (const ws of webSockets) {
+      if (ws?.unique_token == newWs?.unique_token) {
+        isExist = true;
+      }
     }
-  }
 
-  return isExist;
+    return isExist;
+  } catch (error) {
+    throw error;
+  }
 }
 
 subscribeToTicks(channelData);
